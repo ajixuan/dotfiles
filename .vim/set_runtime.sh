53886418d10f1f6cdbdd275c28a3526a191c7e23
Allen Ji <ajixuan11@gmail.com>
2020-09-21 03:06:42 -0400

    install ctags

--- .vim/set_runtime.sh
+++ .vim/set_runtime.sh
@@ -5,6 +5,7 @@ script_dir="$(dirname ${BASH_SOURCE[0]})"
 pathogen_url='https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim'
 nerdtree_url='https://github.com/preservim/nerdtree.git'
 fzf_url='https://github.com/junegunn/fzf.git'
+ctags_url='https://github.com/universal-ctags/ctags.git'
 
 mkdir -p "${script_dir}/autoload"
 mkdir -p "${script_dir}/bundle"
@@ -18,3 +19,13 @@ git clone "${nerdtree_url}" "${script_dir}/bundle/nerdtree"
 
 # fzf
 git clone "${fzf_url}" "${script_dir}/bundle/fzf"
+
+# ctags
+if ! which ctags > /dev/null ; then
+  git clone "${ctags_url}" "${HOME}/tmp"
+  ( cd "${HOME}/tmp" && \
+      "./autogen.sh" && \
+      "./configure" --prefix="${HOME}" && \
+      make && \
+      make install )
+fi
