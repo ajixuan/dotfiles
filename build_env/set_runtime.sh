12a1f6447a94cd8eb35f8d1950bf05a20976c737
Allen Ji <ajixuan11@gmail.com>
2021-08-22 22:49:32 -0400

    setup ghar

--- build_env/set_runtime.sh
+++ build_env/set_runtime.sh
@@ -14,27 +14,34 @@ set -e
 
 # vars
 script_dir="$(dirname ${BASH_SOURCE[0]})"
-work_dir="${script_dir}/../"
+work_dir="${WORK_DIR:-${HOME}}"
 plug_url='https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-rust_analyzer_url='https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux'
+ghar_url='https://github.com/philips/ghar.git'
 
 echo "im a snowman ☃"
 mkdir -p "${work_dir}/.vim/autoload"
 mkdir -p "${work_dir}/.vim/plugged"
 mkdir -p "${work_dir}/.vim/syntax"
 
-# Get Plug
-if [ ! -f "${work_dir}/autoload/plug.vim" ]; then
-  curl -fsSL "${plug_url}" --create-dirs -o "${work_dir}/.vim/autoload/plug.vim"
-fi
-
-# rust analyzer
-if [ ! -f "${HOME}/bin/rust-analyser" ]; then
-  curl -fsSL "${rust_analyzer_url}" --create-dirs -o "${HOME}/bin/rust-analyzer"
-  chmod +x "${HOME}/bin/rust-analyzer"
-fi
-
 if ${build}; then
   echo "Building tools"
   . "${script_dir}/build.sh"
 fi
+
+# Get Plug
+if [ ! -f "${work_dir}/autoload/plug.vim" ]; then
+  echo "Download plug"
+  curl -fsSL "${plug_url}" --create-dirs -o "${work_dir}/.vim/autoload/plug.vim"
+fi
+
+# Install ghar
+if [ ! -f "${work_dir}/bin/ghar" ]; then
+  echo "Download ghar"
+  git_cl "${ghar_url}" "${work_dir}/ghar"
+  ln -s "${work_dir}/ghar/bin/ghar" "${work_dir}/bin/ghar"
+fi
+
+# Install dotfiles
+echo "Installing dotfiles"
+ghar add "$(git remote get-url origin)" mydotfiles
+ghar install mydotfiles
