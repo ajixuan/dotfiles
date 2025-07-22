9006c78821c0b6f10937dfb8c5d19df02a13e697
Allen Ji <ajixuan11@gmail.com>
2025-07-22 00:56:43 -0400

    complete stow setup

--- stow.sh
+++ stow.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-# write the local ignore file
-cat <<EOF > .stow-local-ignore
-.git
-alis
-README.md
-build_env
-ajichangelog.md
-todo.md
-windows.sh
-EOF
-
-# stow all
-stow ./nix
-stow ./vim
-stow .tmux.conf
+stow nix
+stow bash
+stow vim
