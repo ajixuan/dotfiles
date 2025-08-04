d0a3ff127830a27fc15536e0e7b55e4c8ef4feff
Allen Ji <ajixuan11@gmail.com>
2025-08-04 00:08:01 -0400

    nvim builds now

--- build_env/build_env.sh
+++ build_env/build_env.sh
@@ -1,9 +1,15 @@
 ctags_url='https://github.com/universal-ctags/ctags.git'
-cmake_url='https://github.com/Kitware/CMake/releases/download/v3.19.0-rc2/cmake-3.19.0-rc2.tar.gz'
 rust_url='https://sh.rustup.rs'
 ripgrep_url='https://github.com/BurntSushi/ripgrep'
 #tmux_url='https://github.com/tmux/tmux/archive/3.1b.tar.gz'
 tmux_url='https://github.com/tmux/tmux.git'
+fzf_url='https://github.com/junegunn/fzf.git'
+# Get the precompiled nvim
+#nvim_url='https://github.com/neovim/neovim/archive/nightly.tar.gz'
+nvim_url='https://github.com/neovim/neovim.git'
+
+# build dependencies
+cmake_url='https://github.com/Kitware/CMake/archive/refs/tags/v4.1.0-rc4.tar.gz'
 libevent_url='https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz'
 libevent_asc='https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz.asc'
 ncurses_url='https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.4.tar.gz'
@@ -16,13 +22,9 @@ pkgconfig_url='http://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.g
 pkgconfig_asc='https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz.asc'
 bison_url='https://ftp.gnu.org/gnu/bison/bison-3.7.2.tar.gz'
 bison_asc='https://ftp.gnu.org/gnu/bison/bison-3.7.2.tar.gz.sig'
-# Get the precompiled nvim
-#nvim_url='https://github.com/neovim/neovim/archive/nightly.tar.gz'
-nvim_url='https://github.com/neovim/neovim.git'
 m4_url='ftp://ftp.gnu.org:21/gnu/m4/m4-1.4.18.tar.gz'
 libtool_url='https://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz'
 gettext_url='https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz'
-fzf_url='https://github.com/junegunn/fzf.git'
 unzip_url='https://github.com/LuaDist/unzip'
 alacritty_url='https://github.com/alacritty/alacritty/archive/refs/tags/v0.11.0.tar.gz'
 freetype_url='https://download.savannah.gnu.org/releases/freetype/freetype-2.11.1.tar.gz'
