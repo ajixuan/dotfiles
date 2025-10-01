b41d55a5cedad76deabe89ea3e4e5cdb1dc73e65
Allen Ji <ajixuan11@gmail.com>
2025-10-01 17:50:18 +0000

    lsp config setup

--- nix/.config/nvim/lua/plugins/lsp.lua
+++ nix/.config/nvim/lua/plugins/lsp.lua
@@ -19,6 +19,8 @@ return {
       -- - etc
     },
     init = function()
+
+
       vim.g.coq_settings = {
           -- start coq at startup
           auto_start = 'shut-up',
@@ -27,6 +29,40 @@ return {
             jump_to_mark = '',
           }
       }
+
+      local on_attach = function(client, bufnr)
+        -- Mappings
+        local map = vim.keymap.set
+        -- Diagnostic
+        map("n", "gn", vim.diagnostic.goto_next, {desc = "Go to next diagnostics"})
+        map("n", "gb", vim.diagnostic.goto_prev, {desc = "Go to prev diagnostics"})
+        map("n", "ge", vim.diagnostic.open_float, {desc = "Show diagnostics"})
+
+        -- Buffer
+        map('n', 'gd', vim.lsp.buf.definition, {desc ='[LSP] Go to Definition'})
+        map('n', 'gD', vim.lsp.buf.declaration, {desc ='[LSP] Go to Declaration'})
+        map('n', 'gr', vim.lsp.buf.references, {desc = '[LSP] References'})
+        map("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "Go to type definition"})
+      end
+
+      vim.diagnostic.config({
+        virtual_lines = true,
+        virtual_text = true,
+      })
+      vim.lsp.enable('gopls')
+      vim.lsp.config('gopls', {
+        on_attach = on_attach,
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
+          }
+        }
+      })
+
     end,
   }
 }
