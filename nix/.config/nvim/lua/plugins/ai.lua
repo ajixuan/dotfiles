13f4532fb859659bef6c6742da8ee0950026f602
Allen Ji <ajixuan11@gmail.com>
2025-10-19 23:19:11 -0400

    some ai api stuff

--- nix/.config/nvim/lua/plugins/ai.lua
+++ nix/.config/nvim/lua/plugins/ai.lua
@@ -5,10 +5,34 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    opts = {},
-    keys = {
-      {"<leader>n", "<cmd>CodeCompanionChat Toggle<cr>", desc = "AI"}
+    opts = {
+      log_level = "DEBUG",
+      send_code = false,
     },
+    config = function()
+      require("codecompanion").setup({
+        adapters = {
+          http = {
+            venice = function()
+              return require("codecompanion.adapters").extend("deepseek", {
+                name = "venice",
+                formatted_name = "Venice",
+                features = {
+                  text = true,
+                  tokens = true,
+                  vision = false,
+                },
+                env = {
+                  url = "https://api.venice.ai/api",
+                  chat_url = "/v1/chat/completions",
+                  api_key = "cmd: gpg --batch --quiet --decrypt ../llms/venice.gpg"
+                },
+              })
+            end
+          }
+        },
+      })
+    end
   },
 }
 
