9c6b43005a2d4b70f1e682ad9ba4429128446bf8
Allen Ji <ajixuan11@gmail.com>
2025-12-19 02:11:53 +0000

    whichkey comments

--- nix/.config/nvim/lua/plugins/keys.lua
+++ nix/.config/nvim/lua/plugins/keys.lua
@@ -1,17 +1,14 @@
 return  {
   {
     "folke/which-key.nvim",
+    -- Press a starting key and wait, and it will have a popup show up with the key maps available
+    -- This does not work with ctrl however
     event = "VeryLazy",
+
     opts = {
-    },
-    keys ={
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-    },
+       triggers = {
+         { "<auto>", mode = "nixsoc" },
+       }
+    }
   }
 }
