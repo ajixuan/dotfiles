a32e47face4938b20f349726edf69aef4dd15bcb
Allen Ji <ajixuan11@gmail.com>
2026-03-29 20:59:15 +0000

    update

--- nix/.config/nvim/lua/plugins/tree.lua
+++ nix/.config/nvim/lua/plugins/tree.lua
@@ -2,6 +2,9 @@ return {
   {
     'nvim-tree/nvim-tree.lua', version = "*", lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<leader>tf", "<cmd>NvimTreeFindFile<cr>", desc = "Find file in tree" },
+    },
     config = function()
       require("nvim-tree").setup {
         view = {
