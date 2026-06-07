0217f8271d09b6a8829121c49ffb623bdf4bc6b8
Allen Ji <ajixuan11@gmail.com>
2026-06-07 15:21:12 +0000

    better keys

--- nix/.config/nvim/lua/plugins/tree.lua
+++ nix/.config/nvim/lua/plugins/tree.lua
@@ -3,7 +3,21 @@ return {
     'nvim-tree/nvim-tree.lua', version = "*", lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-      { "<leader>tf", "<cmd>NvimTreeFindFile<cr>", desc = "Find file in tree" },
+      { "<Tab>", "<cmd>NvimTreeFindFile<cr>", desc = "Find file in tree" },
+      {
+        "<CR>",
+        function()
+          require("nvim-tree.api").tree.toggle({ find_file = true, focus = true })
+        end,
+        desc = "Toggle tree (reveal current file)",
+      },
+      {
+        "<leader><CR>",
+        function()
+          require("nvim-tree.api").tree.toggle({ find_file = true, focus = true })
+        end,
+        desc = "Toggle tree (reveal current file)",
+      },
     },
     config = function()
       require("nvim-tree").setup {
@@ -12,6 +26,10 @@ return {
           preserve_window_proportions = true,
         },
         sync_root_with_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+        },
         disable_netrw = true,
         git = {
           ignore = false,
