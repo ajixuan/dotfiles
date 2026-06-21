4ba5fbcfabcf2ac56007200672af79fdf86cb4e4
Allen Ji <ajixuan11@gmail.com>
2026-06-21 16:34:44 -0400

    format

---
+++ nix/.config/nvim/lua/plugins/format.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_format" },
+        go = { "gofmt" },
+        rust = { "rustfmt" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+      },
+    },
+    keys = {
+      {
+        "<leader>x",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = { "n", "v" },
+        desc = "Format buffer",
+      },
+    },
+  },
+}
