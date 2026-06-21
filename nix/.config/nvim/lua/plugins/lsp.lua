71a1bdd7c82acbb297f34d65a46e08dd79e38794
Allen Ji <ajixuan11@gmail.com>
2026-06-21 16:08:48 -0400

    nix

--- nix/.config/nvim/lua/plugins/lsp.lua
+++ nix/.config/nvim/lua/plugins/lsp.lua
@@ -56,6 +56,7 @@ return {
   {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
+      "lewis6991/async.nvim",
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
