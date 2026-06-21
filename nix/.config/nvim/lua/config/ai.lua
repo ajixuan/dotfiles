3502bd39199bca5ac14284170281ba729a2f9b8e
Allen Ji <ajixuan11@gmail.com>
2026-06-21 16:31:59 -0400

    remove unused

--- nix/.config/nvim/lua/config/ai.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<leader>ac', ":CodeCompanionChat<CR>" , {desc = 'ai chat'})
-vim.keymap.set('n', '<leader>ai', '', {desc = 'ai inline'})
-vim.keymap.set('n', '<leader>am', ':CodeCompanionCmd<CR>', {desc = 'ai cmd'})
