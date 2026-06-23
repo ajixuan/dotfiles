00d29833ecbd0cce1f179abe084a4f008141c6f9
Allen Ji <ajixuan11@gmail.com>
2026-06-22 23:04:40 -0400

    debug

--- nix/.config/nvim/lua/config/telescope.lua
+++ nix/.config/nvim/lua/config/telescope.lua
@@ -1,10 +1,15 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<C-p>', function()
   builtin.find_files({
     hidden = true,
     file_ignore_patterns = { '%.git/' },
   })
 end, { desc = 'Telescope find files (incl. hidden & ignored)' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+
+vim.keymap.set('n', '<leader>fg', function()
+  builtin.live_grep({ additional_args = { '--hidden', '--glob', '!.git/' } })
+end, { desc = 'Telescope live grep' })
+
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
